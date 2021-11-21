8d768c40d293212bd7c3f1086da858370d11b106
bitaaz <bb_az@yahoo.com>
2021-11-21 16:20:39 +0330

    Add api key to config file

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const API_URL = "https://api.weatherapi.com/v1/"
-const API_KEY = process.env.WEATHER_SITE_API_KEY
-
+// const API_KEY = process.env.REACT_APP_API_KEY
+const REACT_APP_API_KEY = "0103250278524e448d580736212011"
+const API_KEY = REACT_APP_API_KEY
 const CURRENT_WEATHER_CONDITION = `${API_URL}current.json?key=${API_KEY}`
-
 
 export {
     API_URL,
