029bb43221c35ba7c46b79c6b0b09fe4efa2a85b
bitaaz <bb_az@yahoo.com>
2021-11-22 16:39:18 +0330

    Add test for images

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,61 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-    resolve: {
-        fallback: {
-            "fs": false,
-            "tls": false,
-            "net": false,
-            "path": false,
-            "zlib": false,
-            "http": false,
-            "https": false,
-            "stream": false,
-            "crypto": false,
-            "crypto-browserify": require.resolve('crypto-browserify'), //if you want to use this module also don't forget npm i crypto-browserify
-        }
+  resolve: {
+    fallback: {
+      fs: false,
+      tls: false,
+      net: false,
+      path: false,
+      zlib: false,
+      http: false,
+      https: false,
+      stream: false,
+      crypto: false,
+      "crypto-browserify": require.resolve("crypto-browserify"), //if you want to use this module also don't forget npm i crypto-browserify
     },
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: "index.bundle.js",
-        clean: true
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.bundle.js",
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
     },
-    devServer: {
-        static:{
-            directory: path.resolve(__dirname, 'dist')
+    port: 3010,
+    open: true,
+    hot: true,
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
         },
-        port:3010,
-        open: true,
-        hot: true
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'scss-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-        })
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(svg|ico|png|jpg|jpeg|gif)$/,
+        type: "asset/resource",
+      },
     ],
-}
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+};
