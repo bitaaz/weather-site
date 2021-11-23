b5af9dd98f9a69f41d74ad13b67d16e36687da4c
bitaaz <bb_az@yahoo.com>
2021-11-23 16:37:21 +0330

    Add search for cities api

--- src/API.js
+++ src/API.js
@@ -1,10 +1,14 @@
-import {CURRENT_WEATHER_CONDITION} from "./config";
+import { CURRENT_WEATHER_CONDITION, SEARCH_LOCATION } from "./config";
 
 const api_settings = {
-    fetch_current_weather_condition : async (city) => {
-        const endpoint = `${CURRENT_WEATHER_CONDITION}&q=${city}`
-        return await (await fetch(endpoint)).json()
-    }
-}
+  fetch_current_weather_condition: async (city) => {
+    const endpoint = `${CURRENT_WEATHER_CONDITION}&q=${city}`;
+    return await (await fetch(endpoint)).json();
+  },
+  fetch_location: async (location) => {
+    const endpoint = `${SEARCH_LOCATION}&q=${location}`;
+    return await (await fetch(endpoint)).json();
+  },
+};
 
-export default api_settings
+export default api_settings;
