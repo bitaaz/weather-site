0c29a5bbb4b5b91127df0873e3306fdb6dc93afc
bitaaz <bb_az@yahoo.com>
2021-11-24 16:28:12 +0330

    Add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from "react";
 import Home from "./components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { WeatherInfo } from "./components/WeatherInfo";
+
 export function App() {
-    return(
-        <Home/>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/city" element={<WeatherInfo />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
