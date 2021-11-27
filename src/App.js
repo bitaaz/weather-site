835184031b65b5e207ccb3ad01c9b46b34705540
bitaaz <bb_az@yahoo.com>
2021-11-27 16:38:32 +0330

    Change path for current weather element from static to variable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { WeatherInfo } from "./components/WeatherInfo";
+import { CurrentWeatherInfo } from "./components/CurrentWeatherInfo";
 
 export function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/city" element={<WeatherInfo />} />
+        <Route path="/:city" element={<CurrentWeatherInfo />} />
       </Routes>
     </BrowserRouter>
   );
